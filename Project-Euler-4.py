acbc00aabe60e33a572fa6b1fc957a70baf8d044
Timothy Quinn <timothy.quinn@outlook.com>
2014-01-19 00:31:34 +1100

    4th Solution

--- Project-Euler-4.py
+++ Project-Euler-4.py
@@ -1,3 +1,32 @@
-#A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
-#
-#Find the largest palindrome made from the product of two 3-digit numbers.
+import itertools
+
+def palindromeTest(x, y):
+	s = str(x * y)
+	start = s[-int(len(s) / 2):]
+	end = s[:int(len(s) / 2)]
+	if (start == end[::-1]): #string reversal
+		return x * y
+	return None
+
+a = 999
+b = 999
+limit = a - 100
+
+palindromes = []
+listA = []
+listB = []
+while (a > limit):
+	listA.append(a)
+	listB.append(b)
+	a -= 1
+	b -= 1
+
+#cartesian product
+permutations = list(itertools.product(listA, listB))
+
+for pair in permutations:
+	result = palindromeTest(pair[0], pair[1])
+	if (result != None):
+		palindromes.append(result)
+
+print(max(palindromes))
