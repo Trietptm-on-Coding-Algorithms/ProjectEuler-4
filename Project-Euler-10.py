0aabc2dea1de72085d3aacb7c5f7ef0265789105
Timothy Quinn <timothy.quinn@outlook.com>
2014-01-19 21:40:22 +1100

    10th Solution

---
+++ Project-Euler-10.py
@@ -0,0 +1,30 @@
+primes = [2, 3, 5, 7, 11, 13]
+
+def primeTest(num):
+	if ((num % 2) == 0):
+		return False
+	if ((num % 3) == 0):
+		return False
+	x = 5
+	while((x * x) <= (num + 1)): #don't go beyond sqrt of number to save time
+		if (num % x == 0):
+			return False
+		x += 2 #ignore even numbers 
+	return True
+
+x = 17
+while (True):
+	if (x > 2000000):
+		break
+	else:
+		if (primeTest(x) == True):
+			primes.append(x)
+		x += 2
+
+total = 0
+
+for prime in primes:
+	total += int(prime)
+
+print(total)
+
